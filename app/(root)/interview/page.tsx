dcc32414982d9ec331df055577635dcc4819f42a
EddyChan1129 <22026266d@connect.polyu.hk>
2025-04-04 07:12:43 -0400

    Interview UI completed

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,21 +1,34 @@
+import React from 'react'
 import Agent from "@/components/Agent";
-import { getCurrentUser } from "@/lib/actions/auth.action";
 
-const Page = async () => {
-  const user = await getCurrentUser();
+const Page = () => {
+    return (
+        <>
+        <h3>Interview Page</h3>
+            <Agent userName="You" userId="user1" type="generate"/>
+        </>
+    )
+}
+export default Page
 
-  return (
-    <>
-      <h3>Interview generation</h3>
-
-      <Agent
-        userName={user?.name!}
-        userId={user?.id}
-        profileImage={user?.profileURL}
-        type="generate"
-      />
-    </>
-  );
-};
-
-export default Page;
+// import Agent from "@/components/Agent";
+// import { getCurrentUser } from "@/lib/actions/auth.action";
+//
+// const Page = async () => {
+//   const user = await getCurrentUser();
+//
+//   return (
+//     <>
+//       <h3>Interview generation</h3>
+//
+//       <Agent
+//         userName={user?.name!}
+//         userId={user?.id}
+//         profileImage={user?.profileURL}
+//         type="generate"
+//       />
+//     </>
+//   );
+// };
+//
+// export default Page;
